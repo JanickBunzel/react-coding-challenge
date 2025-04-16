3fb407f5d1df9a36f00324793b0186a18fde63ab
Janick Bunzel <janick@bunzel-online.de>
2025-04-16 22:03:06 +0200

    Add Apollo Client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import App from './App.tsx';
+import './index.css';
+
+const apolloClient = new ApolloClient({
+    uri: 'https://swapi-graphql.eskerda.vercel.app/',
+    cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={apolloClient}>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </ApolloProvider>
+);
